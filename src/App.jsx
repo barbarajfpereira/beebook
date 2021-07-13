50272662dab5641d5f67a982bb729a4d6d69b3f1
Barbara Pereira <barbarajfpereira@gmail.com>
2021-07-13 19:36:28 +0100

    add a main display section with animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import './App.scss';
 import Topbar from './components/Topbar/Topbar';
 import Sidebar from './components/Sidebar/Sidebar';
+import MainDisplay from './components/MainDisplay/MainDisplay';
 
 function App() {
   return (
     <div className='app'>
       <Topbar />
-      <div className='main-page'>
+      <div className='main-section'>
         <Sidebar />
-        <div>Welcome to BeeBook</div>
+        <MainDisplay />
       </div>
     </div>
   );
---
+++ src/components/MainDisplay/MainDisplay.jsx
@@ -0,0 +1,11 @@
+import './MainDisplay.scss';
+
+const MainDisplay = () => {
+  return (
+    <div className='main-display'>
+      <div className='welcome-message animation'>Welcome to BeeBook</div>
+    </div>
+  );
+};
+
+export default MainDisplay;
