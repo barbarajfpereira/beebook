1b4c4a9b8fead2a16a546511e156081dd5b6b3e2
Barbara Pereira <barbarajfpereira@gmail.com>
2021-08-04 17:11:19 +0100

    add user's avatar inside user's post

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -3,6 +3,29 @@ import './Posts.scss';
 import { useSelector, useDispatch } from 'react-redux';
 import { setPosts, setLoading, setError } from '../../state/postsSlice';
 import { useHistory } from 'react-router-dom';
+import user1 from '../../assets/user1.png';
+import user2 from '../../assets/user2.png';
+import user3 from '../../assets/user3.png';
+import user4 from '../../assets/user4.png';
+import user5 from '../../assets/user5.png';
+import user6 from '../../assets/user6.png';
+import user7 from '../../assets/user7.png';
+import user8 from '../../assets/user8.png';
+import user9 from '../../assets/user9.png';
+import user10 from '../../assets/user10.png';
+
+const avatars = {
+  1: user1,
+  2: user2,
+  3: user3,
+  4: user4,
+  5: user5,
+  6: user6,
+  7: user7,
+  8: user8,
+  9: user9,
+  10: user10,
+};
 
 const Posts = () => {
   let history = useHistory();
@@ -71,7 +94,12 @@ const Posts = () => {
     <div className='posts'>
       {posts.map(({ userId, title, body }, index) => (
         <div key={index} className='post'>
-          <div className='title'>{`User ${userId}: ${title}`}</div>
+          <img
+            src={avatars[userId]}
+            className='user-avatar'
+            alt='user avatar'
+          />
+          <div className='title'>{title}</div>
           <div className='body'>{body}</div>
         </div>
       ))}
